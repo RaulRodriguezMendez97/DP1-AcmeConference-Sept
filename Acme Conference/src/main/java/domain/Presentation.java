c00ae705bdd2cbb8347c6f6b0d9b3cd68c623fdd
AntSegJim <antonio.alandalus97@gmail.com>
2019-08-15 09:55:12 +0200

    mis clases

--- Acme Conference/src/main/java/domain/Presentation.java
+++ Acme Conference/src/main/java/domain/Presentation.java
@@ -6,14 +6,12 @@ import javax.persistence.AccessType;
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
 import javax.validation.Valid;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Access(AccessType.PROPERTY)
 public class Presentation extends Activity {
 
-	private int			vers;
 	private CamaraReady	camaraReady;
 
 
@@ -26,15 +24,6 @@ public class Presentation extends Activity {
 
 	public void setCamaraReady(final CamaraReady camaraReady) {
 		this.camaraReady = camaraReady;
-	}
-
-	@Min(0)
-	public int getVers() {
-		return this.vers;
-	}
-
-	public void setVers(final int vers) {
-		this.vers = vers;
 	}
 
 }
