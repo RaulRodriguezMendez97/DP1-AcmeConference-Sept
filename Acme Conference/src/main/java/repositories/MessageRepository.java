bdee0ccb30628385c665b59b465284150be0f358
crilorbre <43986572+crilorbre@users.noreply.github.com>
2019-08-19 14:52:50 +0200

    update message finder

--- Acme Conference/src/main/java/repositories/MessageRepository.java
+++ Acme Conference/src/main/java/repositories/MessageRepository.java
@@ -12,7 +12,7 @@ import domain.Message;
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
 
-	@Query("select m from Message m where ((locate(?1,m.sender.email) != 0 or locate(?1,m.receiver.email) != 0) and (locate(?2,m.topic.name) != 0)) ")
+	@Query("select m from Message m where ((locate(?1,m.sender.email) != 0 or locate(?1,m.emailReceiver) != 0) and (locate(?2,m.topic.name) != 0)) ")
 	Collection<Message> getMessagesByFinder(String email, String topic);
 
 }
